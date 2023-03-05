337b636d51d78840eb9a130fd7dd5735e1530712
Tanner Papenfuss <tjpapenfuss@gmail.com>
2023-03-05 12:15:28 -0500

    adding OS_14 folder

---
+++ OS_14/model.py
@@ -0,0 +1,131 @@
+import pandas as pd
+
+#Import the csv files into pandas dataframes.
+df = pd.read_csv("architectures.csv")
+option_df = pd.read_csv("options.csv", index_col=0)
+#df_TradeSpace = df[['Cost', 'Utility']].copy()
+
+# Set the global Variables. These metrics need to be updated in both architectures.csv and options.csv!
+metric1 = "Frequency"
+metric1_weight = 0.15
+
+metric2 = "Data Accuracy"
+metric2_weight = 0.20
+
+metric3 = "Technical Maturity"
+metric3_weight = 0.15
+
+metric4 = "Setup time"
+metric4_weight = 0.20
+
+metric5 = "Safety"
+metric5_weight = 0.15
+
+metric6 = "Human Judgement"
+metric6_weight = 0.15
+
+for item in range(len(df)):
+    # The following code gets the ID (Ie. A2.) for the 10 decisions of the system.
+    # A1/2 are the options for the first architectural decision D1. 
+    #print(a,b,c,d,e,f,g,h,j,k)
+    a_val = df["A"][item]
+    b_val = df["B"][item]
+    c_val = df["C"][item]
+    d_val = df["D"][item]
+    e_val = df["E"][item]
+    f_val = df["F"][item]
+    g_val = df["G"][item]
+    h_val = df["H"][item]
+    j_val = df["J"][item]
+    k_val = df["K"][item]
+
+    ##############################################################################################################################
+    # The following block of code is setting the utility values.
+    # These utilities are calculated according to the following formulas:
+    # U_f=  (2D1+D3+D5 )/4
+    # U_A=  (D1+D2+D6+D8 )/4
+    # U_M=  (D1+D2+D4+2D7+D9)/6
+    # U_T=  (D1+D2+D4+3D6+2D7+2D10)/10
+    # U_S=  (D1+D3+5D10 )/7
+    # U_H=  (D1+D10 )/2
+    # Where D is the decision identifier. Ie. D1 is Measurement collection architecture decision. 
+    ##############################################################################################################################
+    # A block of code
+    # Set the Utility value for the Frequency.
+    #df["Frequency"][item] = ((2*option_df.loc[a_val]["Frequency"]) + \
+    #                        option_df.loc[c_val]["Frequency"] + \
+    #                        option_df.loc[d_val]["Frequency"]) / 4
+    df.at[item, metric1] = round(((2*option_df.loc[a_val][metric1]) + \
+                            option_df.loc[c_val][metric1] + \
+                            option_df.loc[e_val][metric1]) / 4, 2)
+
+    # Set the Utility value for the Accuracy.
+    df.at[item,metric2] = round((option_df.loc[a_val][metric2] + \
+                            option_df.loc[b_val][metric2] + \
+                            option_df.loc[f_val][metric2] + \
+                            option_df.loc[h_val][metric2]) / 4, 2)
+
+    # Set the Utility value for the Technical Maturity.
+    df.at[item,metric3] = round((option_df.loc[a_val][metric3] + \
+                            option_df.loc[b_val][metric3] + \
+                            option_df.loc[d_val][metric3] + \
+                            option_df.loc[j_val][metric3] + \
+                            (2*option_df.loc[g_val][metric3])) / 6, 2)
+    
+    # Set the Utility value for the Setup time.
+    df.at[item,metric4] = round((option_df.loc[a_val][metric4] + \
+                            option_df.loc[b_val][metric4] + \
+                            option_df.loc[d_val][metric4] + \
+                            (3*option_df.loc[f_val][metric4]) + \
+                            (2*option_df.loc[g_val][metric4]) + \
+                            (2*option_df.loc[k_val][metric4])) / 10, 2)
+
+    # Set the Utility value for the Safety.
+    df.at[item,metric5] = round((option_df.loc[a_val][metric5] + \
+                            option_df.loc[c_val][metric5] + \
+                            (5*option_df.loc[k_val][metric5])) / 7, 2)
+
+    # Set the Utility value for the Human judgement.
+    df.at[item,metric6] = round((option_df.loc[a_val][metric6] + \
+                            option_df.loc[k_val][metric6]) / 2, 2)
+
+    # Generate value for the total Utility.
+    # This is calculated using the following equation:
+    # 0.15*U_f + 0.2*U_A + 0.15*U_M + 0.2*U_T + 0.15*U_S + 0.15*U_H
+    df.at[item,"Utility"] = round((metric1_weight*df.loc[item,metric1]) + \
+                            (metric2_weight*df.loc[item,metric2]) + \
+                            (metric3_weight*df.loc[item,metric3]) + \
+                            (metric4_weight*df.loc[item,metric4]) + \
+                            (metric5_weight*df.loc[item,metric5]) + \
+                            (metric6_weight*df.loc[item,metric6]), 2)
+    
+    # Set value for the Cost. 
+    df.at[item,"Cost_norm"] = round((option_df.loc[a_val]["Cost_norm"] + \
+                            option_df.loc[b_val]["Cost_norm"] + \
+                            option_df.loc[c_val]["Cost_norm"] + \
+                            option_df.loc[d_val]["Cost_norm"] + \
+                            option_df.loc[e_val]["Cost_norm"] + \
+                            option_df.loc[f_val]["Cost_norm"] + \
+                            option_df.loc[g_val]["Cost_norm"] + \
+                            option_df.loc[h_val]["Cost_norm"] + \
+                            option_df.loc[j_val]["Cost_norm"] + \
+                            option_df.loc[k_val]["Cost_norm"]) / 10, 2)
+
+    df.at[item,"Cost"] =   round((option_df.loc[a_val]["Cost"] + \
+                            option_df.loc[b_val]["Cost"] + \
+                            option_df.loc[c_val]["Cost"] + \
+                            option_df.loc[d_val]["Cost"] + \
+                            option_df.loc[e_val]["Cost"] + \
+                            option_df.loc[f_val]["Cost"] + \
+                            option_df.loc[g_val]["Cost"] + \
+                            option_df.loc[h_val]["Cost"] + \
+                            option_df.loc[j_val]["Cost"] + \
+                            option_df.loc[k_val]["Cost"]), 0)
+
+
+    #df_TradeSpace.at[item,"Cost"] = df.loc[item,"Cost"]
+    #df_TradeSpace.at[item,"Utility"] = df.loc[item,"Utility"]
+
+df.to_csv("outputTesting.csv")
+
+#df_TradeSpace.to_csv("pythonTrade.csv")
---
+++ OS_14/pythonTrade.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt   
+
+imported_df = pd.read_csv("outputTesting.csv")
+
+
+# Use the below line of code to just plot all points in a scatter plot
+# imported_df.plot(x="Cost", y="Utility", kind="scatter")
+
+# Use this for loop to plot only if some condition is met. 
+# Currently the condition is if K3 is set. 
+for index, row in imported_df.iterrows():
+    if(row['K'] == 'K3'):
+        plt.scatter(row['Cost'], row['Utility'], color="orange")
+    else:
+        plt.scatter(row['Cost'], row['Utility'], color="lightblue")
+
+def pareto_frontier(Xs, Ys, maxX = True, maxY = True):
+    # Sort the list in either ascending or descending order of X
+    myList = sorted([[Xs[i], Ys[i]] for i in range(len(Xs))], reverse=maxX)
+    # Start the Pareto frontier with the first value in the sorted list
+    p_front = [myList[0]]    
+    # Loop through the sorted list
+    for pair in myList[1:]:
+        if maxY:
+            if pair[1] >= p_front[-1][1]: # Look for higher values of Y…
+                p_front.append(pair) # … and add them to the Pareto frontier
+                print(pair)
+        else:
+            if pair[1] <= p_front[-1][1]: # Look for lower values of Y…
+                p_front.append(pair) # … and add them to the Pareto frontier
+                print(pair)
+    # Turn resulting pairs back into a list of Xs and Ys
+    p_frontX = [pair[0] for pair in p_front]
+    p_frontY = [pair[1] for pair in p_front]
+    return p_frontX, p_frontY
+
+# Call the pareto_frontier function with your Cost and utility values from pythonTrade.csv.
+X_values, Y_values = pareto_frontier(imported_df["Cost"], imported_df["Utility"], maxX = False, maxY = True)
+#print(X_values, Y_values) # Print out to pareto frontier values.
+
+ax = plt.gca()
+ax.set_ylim([0, 1]) # Set the y-axis (Utility) limit to 0-1
+plt.scatter(X_values, Y_values, c="red")
+plt.plot(X_values, Y_values, 'red', linestyle="--") # Then plot the Pareto frontier on top of your scatter plot.
+plt.title("Cost vs Utility - MAU Model #2")
+plt.xlabel("Cost ($ Millions)")
+plt.text(300000, 0.9, "Utopia", color="gold")
+plt.plot(100000, 0.90, marker='*', markersize=30, color="gold")
+
+# Plot some reference architectures
+#First Architecture, Semi-Automated Records
+plt.plot(imported_df["Cost"][0], imported_df["Utility"][0], marker='s', markersize=8, color="purple")
+plt.plot(2900000, 0.915, marker='s', markersize=8, color="purple")
+plt.text(1300000, 0.90, "Ref #1: Semi-Automated Records", color="purple")
+
+#Second Architecture, IoT Dashboard Monitoring
+plt.plot(imported_df["Cost"][1], imported_df["Utility"][1], marker='s', markersize=8, color="green")
+plt.plot(2900000, 0.865, marker='s', markersize=8, color="green")
+plt.text(1300000, 0.85, "Ref #2: IoT Dashboard Monitoring", color="green")
+
+#Third Architecture, VR Equipment Inspection
+plt.plot(imported_df["Cost"][2], imported_df["Utility"][2], marker='s', markersize=8, color="darkblue")
+plt.plot(2900000, 0.815, marker='s', markersize=8, color="darkblue")
+plt.text(1300000, 0.80, "Ref #3: VR Equipment Inspection", color="darkblue")
+
+plt.plot(imported_df["Cost"][38], imported_df["Utility"][38], marker='s', markersize=8, color="magenta")
+plt.plot(2900000, 0.765, marker='s', markersize=8, color="magenta")
+plt.text(1300000, 0.75, "Live Video-Sensor Dashboard", color="magenta")
+
+plt.savefig('figure.png') # Save the figure to a file
